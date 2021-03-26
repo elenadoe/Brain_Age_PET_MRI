77714792e84ea3884f62028557c316d4960a9438
elenadoe <elena.doering@uk-koeln.de>
2021-03-26 15:38:57 +0100

    exclude two IDs that could not be pre-processed in PET due to frame issues

--- src/nilearn_parcelation2-mri.py
+++ src/nilearn_parcelation2-mri.py
@@ -5,55 +5,46 @@ import os.path as op
 import numpy as np
 import pandas as pd
 from glob import glob
-from nilearn.datasets import fetch_atlas_schaefer_2018
 
 # Edit paths before running
-subject_list = '../data/OASIS_CN_IDs_Age.txt'
-atlas = fetch_atlas_schaefer_2018(n_rois=200)
-
+subject_list = '/data/project/age_prediction/codes/PET_MRI_age/data/OASIS_PET_IDs.txt'
+atlas_path = '/data/project/age_prediction/codes/PET_MRI_age/data/Schaefer2018_200Parcels_17Networks_order_FSLMNI152_1mm.nii.gz'
 # this should include subjects' folders
-data_path = '/DATA/doeringe/Dokumente/BrainAge/3_SUVR_new/'
-output_csv = '/DATA/doeringe/Dokumente/BrainAge/3_SUVR_new/parcels.csv'
+data_file = '/data/project/cat_12.5/OASIS3'
+output_csv = '/data/project/age_prediction/codes/PET_MRI_age/data/parcels.csv'
 
-# read IDs and age
-subjs = pd.read_csv(subject_list, delimiter="\t")
-
-subjs = pd.read_csv(subject_list, delimiter="\t")
-subj_list = ['sub-' + sub for sub in subjs['SCAN_ID']]
-age = subjs['age']
+# Using readlines()
+file1 = open(subject_list, 'r')
+subjs = file1.read()
+subjs = subjs.splitlines()
+subj_list = ['sub-' + sub for sub in subjs]
+print(subj_list)
 
 count = 0
 image_list = []
 subj_succ = []
-subj_miss = []
-
+# Strips the newline character
 for sub in subj_list:
     sub_name = sub.split("_", 1)[0]
     session = sub.split("_", 1)[1]
-    
-    foi = glob(data_path + "S*" + sub_name + "_" +  session + "*.nii")
+    # /data/project/cat_12.5/HCP/993675/mri/m0wp1993675.nii.gz
+    foi = glob(op.join(data_file, sub_name, session, 'mri', '*.nii*'))
     if foi:
         this_image = nib.load(foi[0])
         niimg = check_niimg(this_image, atleast_4d=True)
-        masker = NiftiLabelsMasker(labels_img=atlas.maps,
+        masker = NiftiLabelsMasker(labels_img=atlas_path,
                                    standardize=False,
                                    memory='nilearn_cache',
                                    resampling_target='data')
         parcelled = masker.fit_transform(niimg)
         image_list.append(parcelled)
         subj_succ.append(sub)
-        
-# exclude data that could not be pre-processed including frame issues
-age = [age[x] for x in range(len(subj_list)) if subj_list[x] in subj_succ]
 
 features = np.array(image_list)
 x, y, z = features.shape
 features = features.reshape(x, z)
-df = pd.DataFrame(features, columns=atlas.labels)
+df = pd.DataFrame(features)
 
-# include age info in dataframe
-subs = {'Subject' : subj_succ,
-       'Age' : age}
-subs_pd = pd.DataFrame(subs)
+subs_pd = pd.DataFrame(subj_succ, columns=['subject'])
 df_new = pd.concat([subs_pd, df], axis=1)
 df_new.to_csv(output_csv, index=False)
