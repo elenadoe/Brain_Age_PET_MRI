d6e1c5792671a8d3f066c1142df5ed0688ccdc32
elenadoe <elena.doering@uk-koeln.de>
2021-04-08 17:23:31 +0200

    adapted to match OASIS files

--- src/1_split_data.py
+++ src/1_split_data.py
@@ -8,9 +8,10 @@ from sklearn.model_selection import train_test_split
 #%matplotlib
 #%config IPCompleter.use_jedi = False
 
-df = pd.read_csv('../data/parcels_FDG_fdgtemplate.csv')
+df = pd.read_csv('../data/OASIS_CN_clean_PET.csv')
+print(df.head())
 
-df['Age_bins'] = df['Age'].values // 5
+df['Age_bins'] = df['age'].values // 5
 df['Age_bins'] = df['Age_bins'].astype(int)
 
 col = [x for x in df.columns if '_' in x]
@@ -18,7 +19,7 @@ col = [x for x in df.columns if '_' in x]
 X = df[col].values
 
 y_pseudo = df['Age_bins']
-y = df['Age']
+y = df['age']
 
 x_train, x_test,  y_train, y_test, id_train, id_test = train_test_split(
     X, y, df['Subject'], test_size=.2, random_state=42,
@@ -33,4 +34,4 @@ plt.hist(y_train // 5)
 plt.hist(y_test // 5)
 plt.show()
 
-df.to_csv('../data/test_train_FDG.csv')
+df.to_csv('../data/test_train_FDG_fdg.csv')
