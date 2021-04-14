9fe769a14f92579263dbf3297264d56f35356ac2
Elena Doering <Elena.Doering@uk-koeln.de>
2021-04-14 17:58:00 +0200

    added dimensionality reduction (PCA)

--- src/2_cv_test_with_PCA.py
+++ src/2_cv_test_with_PCA.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[16]:
 
 
 import pandas as pd
@@ -9,32 +9,57 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import seaborn as sns
+import scipy.stats
 # pip install https://github.com/JamesRitchie/scikit-rvm/archive/master.zip
 from skrvm import RVR
 from sklearn.svm import SVR
-from sklearn.metrics import mean_absolute_error
 from julearn import run_cross_validation
 from julearn.utils import configure_logging
 from nilearn.datasets import fetch_atlas_schaefer_2018
 from nilearn import plotting
 from nilearn import image
+from sklearn.feature_selection import mutual_info_classif, SelectPercentile
 from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.decomposition import PCA
 from sklearn.inspection import permutation_importance
+from sklearn.metrics import mean_absolute_error
 sys.path.append("../lib")
-from lib.create_splits import stratified_splits
+from create_splits import stratified_splits
 
-# configure_logging(level='INFO')
-# TODO: do with FDG template
-# in case seaborn conflicts with autocomplete
-df = pd.read_csv('data/test_train_MRI.csv', index_col=0)
+df = pd.read_csv('../data/test_train_MRI.csv', index_col=0)
 
 df_train = df[df['train'] == "T"]
 # round to no decimal place
 df_train['Age'] = np.around(df_train['Age'].astype(int))
 df_train = df_train.reset_index(drop=True)
-
 col = [x for x in df_train.columns if 'H_' in x]
-plt.hist(y, bins=30)
+plt.hist(df_train['Age'], bins=5)
+
+c = col
+
+# uncomment for PCA
+# [1] creates principal components, 
+# [2] calculates cross products of brain scan and principal components,
+"""pca = PCA()
+pes = pca.fit_transform(df_train[col])
+
+df_pes = pd.DataFrame(pes)
+df_pes.columns = ["PC_"+str(i) for i in range(len(pes))]
+df_pes['Subject'], df_pes['Age'], df_pes['Age_bins'] = df_train['Subject'], df_train['Age'], df_train['Age_bins']
+col1 = [x for x in df_pes.columns if 'PC_' in x]
+col2 = []
+# [3] considers those principal components that are significantly correlated with age
+for i in col1:
+    check = scipy.stats.pearsonr(df_pes[i], df_pes['Age'])[1]
+    if check < 0.1:
+        col2.append(i)
+df_train = df_pes
+c = col2
+"""
+
+
+# In[17]:
+
 
 rand_seed = 42
 num_bins = 5
@@ -52,10 +77,10 @@ res['pred'] = []
 res['ind'] = []
 # res = pd.Series(index=df_train.index)
 for i, model in enumerate(models):
-    cv = StratifiedKFold(n_splits=splits).split(df_train[col],
-                                                df_train['Age'].values.round())
+    cv = StratifiedKFold(n_splits=splits).split(df_train[c],
+                                                df_train['Age_bins'])
     cv = list(cv)
-    scores, final_model = run_cross_validation(X=col, y='Age',
+    scores, final_model = run_cross_validation(X=c, y='Age',
                                          # preprocess_X='scaler_robust',
                                          problem_type='regression',
                                          data=df_train,
@@ -67,7 +92,7 @@ for i, model in enumerate(models):
     model_results.append(final_model)
     scores_results.append(scores)
     for iter in range(splits):
-        pred = scores.estimator[iter].predict(df_train.iloc[cv[iter][1]][col])
+        pred = scores.estimator[iter].predict(df_train.iloc[cv[iter][1]][c])
         res['pred'].append(pred)
         res['iter'].append(iter)
         res['model'].append(str(model))
@@ -89,18 +114,12 @@ df_ages = pd.DataFrame(age_pred)
 y_true = age_pred['real']
 y_pred = age_pred['pred']
 
-# y_pred = final_model.predict(df_train[col])
-# y_true = df_train['Age'].values
-# m, b = np.polyfit(y_true, y_pred, 1)
-# plt.plot(y_true, m*y_true + b)
-
 mae = format(mean_absolute_error(y_true, y_pred), '.2f')
 corr = format(np.corrcoef(y_pred, y_true)[1, 0], '.2f')
 
 fig, ax = plt.subplots(1, 1, figsize=(10, 7))
 sns.set_style("darkgrid")
 plt.scatter(y_true, y_pred)
-# ax = sns.regplot(x=y_true, y=y_pred)
 plt.plot(y_true, y_true)
 xmin, xmax = ax.get_xlim()
 ymin, ymax = ax.get_ylim()
@@ -111,76 +130,73 @@ plt.text(xmax - 0.01 * xmax, ymax - 0.01 * ymax, text, verticalalignment='top',
          horizontalalignment='right', fontsize=12)
 plt.axis('scaled')
 plt.show()
-# In[4]:
+
+
+# In[3]:
 
 
 print("RESULTS")
-print("\nRIDGE VECTOR REGRESSION:\nR2: ", np.round(
-    np.mean(score_results[0]['test_r2']), 2),
-      "\nMAE: ", np.abs(np.round(
-        np.mean(score_results[0]['test_neg_mean_absolute_error']), 2)))
-print("\nSUPPORT VECTOR MACHINE:\nR2: ", np.round(
-    np.mean(score_results[1]['test_r2']), 2),
-      "\nMAE: ", np.abs(np.round(
-        np.mean(score_results[1]['test_neg_mean_absolute_error']), 2)))
+print("\nRIDGE VECTOR REGRESSION:\nR2: ", np.round(np.mean(scores_results[0]['test_r2']),2),
+      "\nMAE: ", np.abs(np.round(np.mean(scores_results[0]['test_neg_mean_absolute_error']),2)))
+print("\nSUPPORT VECTOR MACHINE:\nR2: ", np.round(np.mean(scores_results[1]['test_r2']),2),
+      "\nMAE: ", np.abs(np.round(np.mean(scores_results[1]['test_neg_mean_absolute_error']),2)))
 
 
 # ### Plot Relevant Regions
 
-# In[5]:
+# In[ ]:
 
 
 # permutation importance
-rvr.fit(X, y)
-rvr_feature_importance = permutation_importance(model_results[0]['rvr'], X, y,
-                                                scoring="r2")
-svm = SVR().fit(X, y)
-svr_feature_importance = permutation_importance(svm, X, y, scoring="r2")
-fig, ax = plt.subplots(1, 2, figsize=(15, 5))
-ax[0].bar(range(X.shape[1]), rvr_feature_importance.importances_mean)
-ax[1].bar(range(X.shape[1]), svr_feature_importance.importances_mean)
+rvr.fit(X,y)
+rvr_feature_importance = permutation_importance(model_r[0]['rvr'],X,y, scoring="r2")
+svm = SVR().fit(X,y)
+svr_feature_importance = permutation_importance(svm,X,y, scoring="r2")
+fig,ax = plt.subplots(1,2, figsize = (15,5))
+ax[0].bar(range(X.shape[1]),rvr_feature_importance.importances_mean)
+ax[1].bar(range(X.shape[1]),svr_feature_importance.importances_mean)
 plt.show()
 
 
-# In[6]:
+# In[ ]:
 
 
 # threshold importance indication for better interpretability
-rvr_imp = [x if x >= 0.001 or x < -0.001 else 0 for x in rvr_feature_importance.importances_mean]
-svr_imp = [x if x >= 0.002 else 0 for x in svr_feature_importance.importances_mean]
+rvr_imp = [x if x>=0.001 or x<-0.001 else 0 for x in rvr_feature_importance.importances_mean]
+svr_imp = [x if x>=0.002 else 0 for x in svr_feature_importance.importances_mean]
 
 # labels
-rvr_labels = [col[x] if rvr_imp[x] != 0 else "" for x in range(200)]
-svr_labels = [col[x] if svr_imp[x] != 0 else "" for x in range(200)]
+rvr_labels = [col[x] if rvr_imp[x]!=0 else "" for x in range(200)]
+svr_labels = [col[x] if svr_imp[x]!=0 else "" for x in range(200)]
 
 
-# In[7]:
+# In[ ]:
 
 
-fig, ax = plt.subplots(1, 2, figsize=(15, 5))
-ax[0].bar(range(X.shape[1]), rvr_imp)
+fig,ax = plt.subplots(1,2, figsize = (15,5))
+ax[0].bar(range(X.shape[1]),rvr_imp)
 ax[0].set_title("RVR")
-ax[0].hlines(0, 0, 200, color='black', linewidth=0.5)
+ax[0].hlines(0,0,200,color='black', linewidth = 0.5)
 plt.sca(ax[0])
-plt.xticks(range(200), rvr_labels, rotation=90)
-ax[1].bar(range(X.shape[1]), svr_imp)
+plt.xticks(range(200),rvr_labels, rotation = 90)
+ax[1].bar(range(X.shape[1]),svr_imp)
 ax[1].set_title("SVR")
 plt.sca(ax[1])
-plt.xticks(range(200), svr_labels, rotation=90)
+plt.xticks(range(200),svr_labels, rotation = 90)
 plt.show()
 
 
 # ### Plot Relevant Regions in Glass Brain
 
-# In[8]:
+# In[ ]:
 
 
-schaefer = fetch_atlas_schaefer_2018(n_rois=200, yeo_networks=17)
+schaefer = fetch_atlas_schaefer_2018(n_rois = 200, yeo_networks = 17)
 atlas = image.load_img(schaefer.maps)
 atlas_matrix = image.get_data(atlas)
 
 
-# In[9]:
+# In[ ]:
 
 
 # create statistical map where each voxel value coresponds to permutation importance
@@ -188,60 +204,87 @@ rvr_imp = rvr_feature_importance.importances_mean
 atlas_matrix_stat = atlas_matrix.copy()
 
 for x in range(201):
-    if x == 0:
+    if x == 0: 
         pass
     else:
-        atlas_matrix_stat[atlas_matrix_stat == x] = rvr_imp[x-1]
+        atlas_matrix_stat[atlas_matrix_stat==x] = rvr_imp[x-1]
 atlas_rvr = image.new_img_like(atlas, atlas_matrix_stat)
 
 
-# In[10]:
+# In[ ]:
 
 
 svr_imp = svr_feature_importance.importances_mean
 atlas_matrix_stat_svr = atlas_matrix.copy()
 
 for x in range(201):
-    if x == 0:
+    if x == 0: 
         pass
     else:
-        atlas_matrix_stat_svr[atlas_matrix_stat_svr == x] = svr_imp[x-1]
+        atlas_matrix_stat_svr[atlas_matrix_stat_svr==x] = svr_imp[x-1]
 atlas_svr = image.new_img_like(atlas, atlas_matrix_stat_svr)
 
 
-# In[11]:
+# In[ ]:
 
 
-fix, ax = plt.subplots(1, 2, figsize=(15, 5))
-plotting.plot_stat_map(atlas_rvr, axes=ax[0])
+fix, ax = plt.subplots(1,2, figsize=(15,5))
+plotting.plot_stat_map(atlas_rvr, axes = ax[0])
 ax[0].set_title("RVR-relevant regions for aging")
-plotting.plot_stat_map(atlas_svr, axes=ax[1])
+plotting.plot_stat_map(atlas_svr, axes = ax[1])
 ax[1].set_title("SVR-relevant regions for aging")
 plt.show()
 
 
 # ### Performance on test set
 
-# In[25]:
+# In[4]:
 
 
 df_test = df[df['train'] == "F"]
-col = [x for x in df_train.columns if 'H_' in x]
+df_test['Age'] = np.around(df_test['Age'].astype(int))
+df_test = df_test.reset_index(drop=True)
 
-X_test = df_test[col].values
-# y_pseudo = df_test['Age_bins']
+col = [x for x in df_test.columns if 'H_' in x]
+
 y_test = df_test['Age'].values
 # round to no decimal place
 y_test = np.around(y_test).astype(int)
 
 
-# In[26]:
+# In[5]:
 
 
-pred = model_results[0]['rvr'].predict(X_test)
+pred = model_results[0]['rvr'].predict(df_test[col])
+# uncomment for PCA
+"""
+pes_test = pca.transform(df_test[col].values)
+pred = model_results[0]['rvr'].predict(np.array(pes_test)[:,col2_ind])
+"""
+mae = format(mean_absolute_error(y_test, pred), '.2f')
+corr = format(np.corrcoef(y_test, pred)[1, 0], '.2f')
+
+print(len(pred),len(y_test))
+fig, ax = plt.subplots(1, 1, figsize=(10, 7))
 plt.scatter(y_test, pred)
+plt.plot(y_test,y_test)
 plt.xlabel("Ground truth")
 plt.ylabel("RVR Prediction")
-plt.xlim(50, 80)
-plt.ylim(50, 80)
+plt.xlim(50,80)
+plt.ylim(50,80)
+text = 'MAE: ' + str(mae) + '   CORR: ' + str(corr)
+ax.set(xlabel='True values', ylabel='Predicted values')
+plt.title('Actual vs Predicted')
+xmin, xmax = ax.get_xlim()
+ymin, ymax = ax.get_ylim()
+plt.text(xmax - 0.01 * xmax, ymin + 0.05 * ymin, text, verticalalignment='top',
+         horizontalalignment='right', fontsize=12)
+
 plt.show()
+
+
+# In[ ]:
+
+
+
+
