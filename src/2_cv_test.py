952e1902b867ed4ab238f1f265632cd72a4292d7
Elena Doering <Elena.Doering@uk-koeln.de>
2021-05-31 17:38:26 +0200

    added permutation importance

--- src/2_cv_test.py
+++ src/2_cv_test.py
@@ -1,37 +1,25 @@
-# In[1]:
-
-
+#%%
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
-import seaborn as sns
-#pip install https://github.com/JamesRitchie/scikit-rvm/archive/master.zip
+import plots
 from skrvm import RVR
-from sklearn.svm import SVR
-from sklearn.metrics import mean_absolute_error
 from julearn import run_cross_validation
-from julearn.utils import configure_logging
-from nilearn.datasets import fetch_atlas_schaefer_2018
-from nilearn import plotting
-from nilearn import image
-from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.model_selection import StratifiedKFold
 from sklearn.inspection import permutation_importance
 from sklearn.linear_model import LinearRegression
 
-
-# configure_logging(level='INFO')
-# TODO: do with FDG template
-# in case seaborn conflicts with autocomplete
-
+# %%
+modality = "MRI"
+mode = "train"
+df = pd.read_csv('../data/ADNI/test_train_MRI_ADNI.csv')
 df_train = df[df['train'] == "T"]
 col = [x for x in df_train.columns if ('_' in x)]
 
 # round to no decimal place
 df_train = df_train.reset_index(drop=True)
 
-#col = [x for x in df_train.columns if 'H_' in x]
-plt.hist(y, bins=30)
+plt.hist(df_train['Age'], bins=30)
 
 rand_seed = 42
 num_bins = 5
@@ -47,7 +35,7 @@ res['model'] = []
 res['iter'] = []
 res['pred'] = []
 res['ind'] = []
-# res = pd.Series(index=df_train.index)
+
 for i, model in enumerate(models):
     cv = StratifiedKFold(n_splits=splits).split(df_train[col],
                                                 df_train['Agebins'])
@@ -84,36 +72,20 @@ for i, fold in enumerate(df_res['ind']):
         age_pred['model'].append(df_res.iloc[i]['model'])
 
 df_ages = pd.DataFrame(age_pred)
-
+# %%
 y_true = df_ages[df_ages['model'] == 'svm']['real']
 y_pred = df_ages[df_ages['model'] == 'svm']['pred']
 
 # fit a linear model for bias correction
+# TODO: bias correction without chronological age
 lm_rvr = LinearRegression()
 lm_rvr.fit(np.array(y_true).reshape(-1,1), np.array(y_pred-y_true).reshape(-1,1))
 slope_rvr = lm_rvr.coef_
 intercept_rvr = lm_rvr.intercept_
 y_pred_bc = y_pred - (y_true*slope_rvr[0][0]+intercept_rvr[0])
-#y_pred_bc = y_pred
 
-mae = format(mean_absolute_error(y_true, y_pred_bc), '.2f')
-corr = format(np.corrcoef(y_pred_bc, y_true)[1, 0], '.2f')
-
-fig, ax = plt.subplots(1, 1, figsize=(10, 7))
-sns.set_style("darkgrid")
-plt.scatter(y_true, y_pred_bc)
-m, b = np.polyfit(y_true, y_pred_bc, 1)
-plt.plot(y_true, m*y_true + b)
-xmin, xmax = ax.get_xlim()
-ymin, ymax = ax.get_ylim()
-text = 'MAE: ' + str(mae) + '   CORR: ' + str(corr)
-ax.set(xlabel='True values', ylabel='Predicted values')
-plt.title('Actual vs Predicted - SVR')
-plt.text(xmin + 10, ymax - 0.01 * ymax, text, verticalalignment='top',
-         horizontalalignment='right', fontsize=12)
-#plt.savefig("../results/train_performance_svm_FDG_biascorr.jpg")
-plt.show()
-
+# plot real_vs_pred
+plots.real_vs_pred(y_true,y_pred_bc, mode, "rvr", modality)
 
 y_true = df_ages[df_ages['model'] == 'RVR()']['real']
 y_pred = df_ages[df_ages['model'] == 'RVR()']['pred']
@@ -124,152 +96,35 @@ lm_svr.fit(np.array(y_true).reshape(-1,1), np.array(y_pred-y_true).reshape(-1,1)
 slope_svr = lm_svr.coef_
 intercept_svr = lm_svr.intercept_
 y_pred_bc = y_pred - (y_true*slope_svr[0]+intercept_svr[0])
-#y_pred_bc = y_pred
 
-mae = format(mean_absolute_error(y_true, y_pred_bc), '.2f')
-corr = format(np.corrcoef(y_pred_bc, y_true)[1, 0], '.2f')
+# plot real_vs_pred
+plots.real_vs_pred(y_true,y_pred_bc, mode, "svr", modality)
 
-fig, ax = plt.subplots(1, 1, figsize=(10, 7))
-sns.set_style("darkgrid")
-plt.scatter(y_true, y_pred_bc)
-m, b = np.polyfit(y_true, y_pred_bc, 1)
-plt.plot(y_true, m*y_true + b)
-
-xmin, xmax = ax.get_xlim()
-ymin, ymax = ax.get_ylim()
-text = 'MAE: ' + str(mae) + '   CORR: ' + str(corr)
-ax.set(xlabel='True values', ylabel='Predicted values')
-plt.title('Actual vs Predicted - RVR')
-plt.text(xmin + 10, ymax - 0.01 * ymax, text, verticalalignment='top',
-         horizontalalignment='right', fontsize=12)
-#plt.savefig("../results/train_performance_rvr_FDG_biascorr.jpg")
-plt.show()
-
-y_true = df_ages[df_ages['model'] == 'gauss']['real']
-y_pred = df_ages[df_ages['model'] == 'gauss']['pred']
-
-# fit a linear model for bias correction
-lm_gauss = LinearRegression()
-lm_gauss.fit(np.array(y_true).reshape(-1,1), np.array(y_pred-y_true).reshape(-1,1))
-slope_gauss = lm_gauss.coef_
-intercept_gauss = lm_gauss.intercept_
-y_pred_bc = y_pred - (y_true*slope_gauss[0][0]+intercept_gauss[0])
-#y_pred_bc = y_pred
-
-mae = format(mean_absolute_error(y_true, y_pred_bc), '.2f')
-corr = format(np.corrcoef(y_pred_bc, y_true)[1, 0], '.2f')
-
-fig, ax = plt.subplots(1, 1, figsize=(10, 7))
-sns.set_style("darkgrid")
-plt.scatter(y_true, y_pred_bc)
-m, b = np.polyfit(y_true, y_pred_bc, 1)
-plt.plot(y_true, m*y_true + b)
-xmin, xmax = ax.get_xlim()
-ymin, ymax = ax.get_ylim()
-text = 'MAE: ' + str(mae) + '   CORR: ' + str(corr)
-ax.set(xlabel='True values', ylabel='Predicted values')
-plt.title('Actual vs Predicted - Gaussian')
-plt.text(xmin + 10, ymax - 0.01 * ymax, text, verticalalignment='top',
-         horizontalalignment='right', fontsize=12)
-#plt.savefig("../results/train_performance_gauss_FDG_biascorr.jpg")
-plt.show()
-
-# PERMUTATION IMP
-rvr.fit(X, y)
-rvr_feature_importance = permutation_importance(model_results[0]['rvr'], X, y,
-                                                scoring="r2")
-svm = SVR().fit(X, y)
-svr_feature_importance = permutation_importance(svm, X, y, scoring="r2")
-fig, ax = plt.subplots(1, 2, figsize=(15, 5))
-ax[0].bar(range(X.shape[1]), rvr_feature_importance.importances_mean)
-ax[1].bar(range(X.shape[1]), svr_feature_importance.importances_mean)
-plt.show()
-
-schaefer = fetch_atlas_schaefer_2018(n_rois=200, yeo_networks=17)
-atlas = image.load_img(schaefer.maps)
-atlas_matrix = image.get_data(atlas)
-
-# create statistical map where each voxel value coresponds to permutation importance
-rvr_imp = rvr_feature_importance.importances_mean
-atlas_matrix_stat = atlas_matrix.copy()
-
-for x in range(201):
-    if x == 0:
-        pass
-    else:
-        atlas_matrix_stat[atlas_matrix_stat == x] = rvr_imp[x-1]
-atlas_rvr = image.new_img_like(atlas, atlas_matrix_stat)
-
-svr_imp = svr_feature_importance.importances_mean
-atlas_matrix_stat_svr = atlas_matrix.copy()
-atlas_svr_dict = {}
-
-for x in range(201):
-    if x == 0:
-        pass
-    else:
-        atlas_matrix_stat_svr[atlas_matrix_stat_svr == x] = svr_imp[x-1]
-        atlas_svr_dict[x] = svr_imp[x-1]
-atlas_svr = image.new_img_like(atlas, atlas_matrix_stat_svr)
-
-fig, ax = plt.subplots(1, 2, figsize=(15, 5))
-plotting.plot_stat_map(atlas_rvr, axes=ax[0])
-ax[0].set_title("RVR-relevant regions for aging")
-plotting.plot_stat_map(atlas_svr, axes=ax[1])
-ax[1].set_title("SVR-relevant regions for aging")
-plt.savefig("../results/Permutation_importance_FDG.jpg")
-#nib.save(atlas_rvr,"../results/permutation_importance_RVR_FDG.nii")
-#nib.save(atlas_svr,"../results/permutation_importance_SVR_FDG.nii")
-plt.show()
-
+# %%
 # TESTING
 df_test = df[df['train'] == "F"]
+mode = "test"
 col = [x for x in df_train.columns if '_' in x]
 
 X_test = df_test[col].values
 y_true = df_test['Age'].values
 
-# In[26]:
-
-
 y_pred = model_results[0]['rvr'].predict(X_test)
 
 y_pred_bc = y_pred - (y_true*slope_rvr[0]+intercept_rvr[0])
-#y_pred_bc = y_pred
 
-mae = format(mean_absolute_error(y_true, y_pred_bc), '.2f')
-corr = format(np.corrcoef(y_pred_bc, y_true)[1, 0], '.2f')
-
-plt.scatter(y_true, y_pred_bc)
-sns.set_style("darkgrid")
-plt.xlabel("Ground truth")
-plt.ylabel("RVR Prediction")
-text = 'MAE: ' + str(mae) + '   CORR: ' + str(corr)
-plt.title('Actual vs Predicted - Test Set')
-plt.text(75, 88, text, verticalalignment='top',
-         horizontalalignment='right', fontsize=12)
-plt.xlim(55,90)
-plt.ylim(55,90)
-plt.savefig("../results/test_performance_rvr_MRI_biascorr.jpg")
-plt.show()
+plots.real_vs_pred(y_true,y_pred_bc, mode, "rvr", modality)
 
 y_pred = model_results[1]['svm'].predict(X_test)
-
 y_pred_bc = y_pred - (y_true*slope_svr[0]+intercept_svr[0])
-#y_pred_bc = y_pred
 
-mae = format(mean_absolute_error(y_true, y_pred_bc), '.2f')
-corr = format(np.corrcoef(y_pred_bc, y_true)[1, 0], '.2f')
+plots.real_vs_pred(y_true,y_pred_bc, mode, "svr", modality)
+# %%
+# PERMUTATION IMP
+rvr_feature_importance = permutation_importance(model_results[0]['rvr'], X_test, y_true,
+                                                scoring="r2", n_repeats = 1000)
+svr_feature_importance = permutation_importance(model_results[1]['svm'], X_test, y_true, scoring="r2", n_repeats = 1000)
 
-plt.scatter(y_true, y_pred_bc)
-sns.set_style("darkgrid")
-plt.xlabel("Ground truth")
-plt.ylabel("SVR Prediction")
-text = 'MAE: ' + str(mae) + '   CORR: ' + str(corr)
-plt.title('Actual vs Predicted - Test Set')
-plt.text(75, 88, text, verticalalignment='top',
-         horizontalalignment='right', fontsize=12)
-plt.xlim(55,90)
-plt.ylim(55,90)
-plt.savefig("../results/test_performance_svm_MRI_biascorr.jpg")
-plt.show()
+
+
+# %%
--- src/plots.py
+++ src/plots.py
@@ -1,15 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import nibabel as nib
+from nilearn.datasets import fetch_atlas_schaefer_2018
+from nilearn import plotting
+from nilearn import image
 from nilearn import plotting
 from nilearn import image
 from sklearn.metrics import mean_absolute_error
 
-def real_vs_pred(y_true, y_pred)
+# plot ground truth against predictions
+def real_vs_pred(y_true, y_pred, alg, modality, train_test):
     """Plots True labels against the predicted ones.
     inputs: 
     y_true = list of floating point values or integers, representing ground truth values
     y_pred = list of floating point values or integers, representing predicted values
+    alg = algorithm used for current task (used for saving)
+    modality = modality with which brain age was assessed (MRI/PET; used for saving)
+    train_test = str indicating whether train or test data is plotted (used for saving)
+    
+    outputs: 
+    none (plots and saves plots)
     """
     mae = format(mean_absolute_error(y_true, y_pred), '.2f')
     corr = format(np.corrcoef(y_pred, y_true)[1, 0], '.2f')
@@ -25,6 +36,37 @@ def real_vs_pred(y_true, y_pred)
     plt.title('Actual vs Predicted')
     plt.text(xmin + 10, ymax - 0.01 * ymax, text, verticalalignment='top',
             horizontalalignment='right', fontsize=12)
+    plt.savefig("../results/real_vs_pred_{}_{}_{}.jpg".format(train_test, modality, alg))
     plt.show()
 
 # plot permutation importance
+def permutation_imp(feature_imp, alg, modality):
+    """Plots permutation importance as evaluated in test set
+    inputs: 
+    feature_imp = dictionary-like object from calling sklearn.inspection.permutation_importance
+    alg = algorithm used for current task (used for saving)
+    modality = modality with which brain age was assessed (MRI/PET; used for saving)
+        
+    outputs: 
+    none (plots and saves plots)
+    """
+    schaefer = fetch_atlas_schaefer_2018(n_rois=200, yeo_networks=17)
+    atlas = image.load_img(schaefer.maps)
+    atlas_matrix = image.get_data(atlas)
+
+    # create statistical map where each voxel value coresponds to permutation importance
+    imp = feature_imp.importances_mean
+    atlas_matrix_stat = atlas_matrix.copy()
+
+    for x in range(201):
+        if x == 0:
+            pass
+        else:
+            atlas_matrix_stat[atlas_matrix_stat == x] = imp[x-1]
+    atlas_final = image.new_img_like(atlas, atlas_matrix_stat)
+
+    plotting.plot_stat_map(atlas_final)
+    plt.title("{}-relevant regions for aging".format(alg))
+    plt.savefig("../results/Permutation_importance_{}_{}.jpg".format(modality, alg))
+    nib.save(atlas_final,"../results/permutation_importance_{}_{}.nii".format(modality, alg))
+    plt.show()
