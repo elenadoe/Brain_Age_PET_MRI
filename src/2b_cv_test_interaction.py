409105601800d5c9e00924ddfd8f5861228b3fab
antogeo <g.antonopoulos@fz-juelich.de>
2021-11-02 12:47:51 +0100

    edit features for interaction effect

--- src/2b_cv_test_interaction.py
+++ src/2b_cv_test_interaction.py
@@ -1,19 +1,19 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import src.plots as src.plots
+import plots as plots
 from sklearn.inspection import permutation_importance
 from skrvm import RVR
 from julearn import run_cross_validation
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
-
+cd src
 # %%
 modality = "multimodal"
 mode = "train"
-mri = pd.read_csv('data/ADNI/test_train_PET_NP_amytau_olderthan65_42.csv')
-pet = pd.read_csv('data/ADNI/test_train_MRI_NP_amytau_olderthan65_42.csv')
+mri = pd.read_csv('../data/ADNI/test_train_PET_NP_amytau_olderthan65_42.csv')
+pet = pd.read_csv('../data/ADNI/test_train_MRI_NP_amytau_olderthan65_42.csv')
 mri_train = mri[mri['train'] == True]
 pet_train = pet[pet['train'] == True]
 mri_train = mri_train.reset_index()
@@ -37,9 +37,28 @@ plt.show()
 rand_seed = 42
 num_bins = 5
 rvr = RVR()
-models = [rvr, 'svm', 'gauss']
-model_names = ['rvr', 'svm', 'gauss']
+
+models = [rvr, 'svm', 'gradientboost']
+model_names = ['rvr', 'svm', 'gradientboost']
 splits = 5
+
+# hyperparameters svr & rvr
+kernels = ['linear', 'rbf']
+cs = [0.001, 0.01, 0.1, 1, 10, 100]
+# hyperparameters gb
+loss = ['squared_error', 'absolute_error']
+n_estimators = [10, 100, 1000]
+learning_rate = [0.001, 0.01, 0.1]
+
+model_params = [{'rvr__C': cs, 'rvr__kernel': kernels},
+                {'svm__C': cs, 'svm__kernel': kernels},
+                {'gradientboost__n_estimators': n_estimators,
+                 'gradientboost__learning_rate': learning_rate}]
+
+
+df_train = pd.DataFrame(pet_train[col].values*mri_train[col].values,
+                        columns=col, index=pet_train.index)
+df_interact = pd.concat([mri_train.drop(col, axis=1), df_train], axis=1)
 
 model_results = []
 scores_results = []
@@ -48,30 +67,15 @@ res['model'] = []
 res['iter'] = []
 res['pred'] = []
 res['ind'] = []
-
-mri_scaler = StandardScaler()
-pet_scaler = StandardScaler()
-#  according to the columns (pet_train or mri_train):
-mri_train_data = mri_scaler.fit_transform(mri_train[col])
-# KeyError: "['ATHA_rh', 'AMY_lh', 'PUT_lh', 'PTHA_rh'] not in index"
-pet_train_data = pet_scaler.fit_transform(pet_train[col])
-# KeyError: "['aTHA_rh', 'pTHA_rh', 'AMY_h', 'PUT_h'] not in index"
-interact_train = mri_train_data * pet_train_data
-interact_train = pd.DataFrame(interact_train, columns=col)
-print(interact_train.shape)
-
-interact_train['name'] = mri_train['name']
-interact_train['Agebins'] = mri_train['Agebins']
-interact_train['age'] = mri_train['age']
-print(interact_train.shape)
 # %%
 for i, model in enumerate(models):
-    cv = StratifiedKFold(n_splits=splits).split(interact_train[col],
-                                                interact_train['Agebins'])
+    cv = StratifiedKFold(n_splits=splits).split(df_interact[col],
+                                                df_interact['Agebins'])
     cv = list(cv)
-    scores, final_model = run_cross_validation(X=col, y='Age',
+    scores, final_model = run_cross_validation(X=col, y='age',
+                                               preprocess_X='scaler_robust',
                                                problem_type='regression',
-                                               data=interact_train,
+                                               data=df_interact,
                                                model=model, cv=cv,
                                                return_estimator='all',
                                                seed=rand_seed,
@@ -81,7 +85,7 @@ for i, model in enumerate(models):
     scores_results.append(scores)
     for iter in range(splits):
         pred = scores.estimator[iter].predict(
-                        interact_train.iloc[cv[iter][1]][col])
+                        df_interact.iloc[cv[iter][1]][col])
         res['pred'].append(pred)
         res['iter'].append(iter)
         res['model'].append(str(model))
@@ -95,9 +99,9 @@ age_pred['real'] = []
 age_pred['model'] = []
 for i, fold in enumerate(df_res['ind']):
     for ind, sample in enumerate(fold):
-        age_pred['real'].append(interact_train.iloc[sample]['Age'])
+        age_pred['real'].append(df_interact.iloc[sample]['age'])
         age_pred['pred'].append(df_res['pred'].iloc[i][ind])
-        age_pred['subj'].append(interact_train.iloc[sample]['Subject'])
+        age_pred['subj'].append(df_interact.iloc[sample]['name'])
         age_pred['model'].append(df_res.iloc[i]['model'])
 
 df_ages = pd.DataFrame(age_pred)
@@ -120,11 +124,11 @@ y_true = df_ages[df_ages['model'] == 'RVR()']['real']
 y_pred = df_ages[df_ages['model'] == 'RVR()']['pred']
 
 # fit a linear model for bias correction
-lm_svr = LinearRegression()
-lm_svr.fit(np.array(y_pred).reshape(-1, 1), np.array(y_true).reshape(-1, 1))
-slope_svr = lm_svr.coef_
-intercept_svr = lm_svr.intercept_
-y_pred_bc = (y_pred - intercept_svr[0])/slope_svr[0][0]
+lm_rvr = LinearRegression()
+lm_rvr.fit(np.array(y_pred).reshape(-1, 1), np.array(y_true).reshape(-1, 1))
+slope_rvr = lm_rvr.coef_[0][0]
+intercept_rvr = lm_rvr.intercept_[0]
+y_pred_bc = (y_pred - intercept_rvr)/slope_rvr
 
 
 # plot real_vs_pred
@@ -134,21 +138,34 @@ plots.real_vs_pred(y_true, y_pred_bc, "svr", mode, modality)
 # TESTING
 
 # TODO: df appears first here. The same df_train some line bellow!
-df_test = df[df['train'] == "F"]
-mode = "test"
-col = [x for x in df_train.columns if '_' in x]
+mri_test = mri[mri['train'] == False]
+pet_test = pet[pet['train'] == False]
+mri_test = mri_test.reset_index()
+pet_test = pet_test.reset_index()
+print(mri_test.shape)
 
-X_test = df_test[col].values
-y_true = df_test['Age'].values
+
+df_test = pd.DataFrame(pet_test[col].values*mri_test[col].values,
+                       columns=col, index=pet_test.index)
+df_interact_test = pd.concat([mri_test.drop(col, axis=1), df_test], axis=1)
+# check that all IDs are the same
+
+# changed "Subject" to "name"
+if pet_test['name'].equals(mri_test['name']):
+    print("Subjects in two modalities match")
+mode = "test"
+
+X_test = df_interact_test[col].values
+y_true = df_interact_test['age'].values
 
 y_pred = model_results[0]['rvr'].predict(X_test)
 
-y_pred_bc = y_pred - (y_true*slope_rvr[0]+intercept_rvr[0])
+y_pred_bc = y_pred - (y_true*slope_rvr+intercept_rvr)
 
 plots.real_vs_pred(y_true, y_pred_bc, mode, "rvr", modality)
 
 y_pred = model_results[1]['svm'].predict(X_test)
-y_pred_bc = y_pred - (y_true*slope_svr[0]+intercept_svr[0])
+y_pred_bc = y_pred - (y_true*slope_svr+intercept_svr)
 
 plots.real_vs_pred(y_true, y_pred_bc, mode, "svr", modality)
 # %%
