ef5a8e0192e6e7c040432a66b866ea390e07dc52
Elena Doering <doeringe@mmni-compute>
2021-11-24 17:02:45 +0100

    add with corrected new stratification into youngest-old, middle old and oldest-old

--- src/1_split_data.py
+++ src/1_split_data.py
@@ -1,38 +1,42 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-modality = 'MRI'
-df = pd.read_csv('../data/ADNI/ADNI_'+modality+'_Sch_Tian_1mm_parcels.csv',
+df_mri = pd.read_csv('../data/ADNI/ADNI_MRI_Sch_Tian_1mm_parcels.csv',
                  sep = ",")
-df_con = pd.read_csv('../data/ADNI/ADNI_PET_Sch_Tian_1mm_parcels.csv',
+df_pet = pd.read_csv('../data/ADNI/ADNI_PET_Sch_Tian_1mm_parcels.csv',
                  sep = ";")
-print(all(df['name'] == df_con['name']))
+print(all(df_mri['name'] == df_pet['name']))
 #%%
-df = df[df['age']>=65]
-df = df.reset_index(drop=True)
-df_con = df[df['age']>=65]
-df_con = df_con.reset_index(drop=True)
-df = df[df_con['age']>=65]
-df = df.reset_index(drop=True)
-print("Individuals younger than 65 contained in data:",any(df['age']<65))
+df_mri = df_mri[df_mri['age']>=65]
+df_pet = df_mri[df_mri['age']>=65]
+df_mri = df_mri.reset_index(drop=True)
+df_pet = df_pet.reset_index(drop=True)
 
+df_mri = df_mri[df_pet['age']>=65]
+df_mri = df_mri.reset_index(drop=True)
+df_pet = df_pet.reset_index(drop=True)
+print("Individuals younger than 65 contained in data:",any(df_mri['age']<65))
+print("Same IDs contained in both dataframes:", all(df_mri['name'] == df_pet['name']))
 #%%
 # divide into age bins of "young old", "middle old" and "oldest old"
-df['Agebins'] = [0 if x < 74 else 1 if x < 84 else 2 for x in df['age']]
-#df['Agebins'] = df['Agebins'].astype(int)
+df_mri['Agebins'] = [0 if x < 74 else 1 if x < 84 else 2 for x in df_mri['age']]
+df_pet['Agebins'] = [0 if x < 74 else 1 if x < 84 else 2 for x in df_pet['age']]
 
-col = df.columns[3:-1].tolist()
+col = df_mri.columns[3:-1].tolist()
 
-X = df[col].values
-y = df['age'].values
+X = df_mri[col].values
+y = df_mri['age'].values
 #%%
-y_pseudo = df['Agebins']
+y_pseudo = df_mri['Agebins']
 
 x_train, x_test,  y_train, y_test, id_train, id_test = train_test_split(
-    X, y, df['name'], test_size=.2, random_state=0,
+    X, y, df_mri['name'], test_size=.2, random_state=42,
     stratify=y_pseudo)
 
-df['train'] = [True if x in id_train.values else False for x in df[
+df_mri['train'] = [True if x in id_train.values else False for x in df_mri[
                'name']]
-
-df.to_csv('../data/ADNI/test_train_'+modality+'.csv')
+df_pet['train'] = [True if x in id_train.values else False for x in df_pet[
+               'name']]
+print(all((df_mri['train'] == df_pet['train']) and ))
+df_mri.to_csv('../data/ADNI/test_train_MRI.csv')
+df_pet.to_csv('../data/ADNI/test_train_PET.csv')
