74d13e3b08dd4a285267d04ea5db311b83133fef
Elena Doering <Elena.Doering@uk-koeln.de>
2021-12-20 18:57:58 +0100

    exclude outliers with ranges inferred from train set

--- src/1.1_neuropsychology_merge/neuropsychology_merge.R
+++ src/1.1_neuropsychology_merge/neuropsychology_merge.R
@@ -5,7 +5,7 @@ adni <- adni[(adni$DX == "CN"),]
 
 rownames(adni) <- NULL
 
-data <- read.csv('C:/Users/doeringe/Documents/BrainAge/PET_MRI_age/data/ADNI/test_train_MRI.csv')
+data <- read.csv('C:/Users/doeringe/Documents/BrainAge/PET_MRI_age/data/merged/test_train_PET_42.csv')
 
 adni <- subset(adni, select = c(PTID, MMSE, ABETA, PTAU, TAU, AV45, CDRSB, ADAS13, RAVLT_immediate, RAVLT_learning, RAVLT_forgetting,
                        FAQ, MOCA, EcogPtMem, EcogPtLang, EcogPtVisspat, EcogPtPlan,
@@ -16,6 +16,7 @@ names(adni)[names(adni) == "PTID"] <- "name"
 df <- merge(data,adni,all.x=TRUE,all.y=FALSE)
 
 df$ABETA[df$ABETA == ">1700"] <- 1700
-df$TAU[df$TAU == '<8'] <- 8
+df$TAU[df$TAU == '<80'] <- 80
+df$TAU[df$PTAU == '<8'] <- 8
 
-write.csv(df, "C:/Users/doeringe/Documents/BrainAge/PET_MRI_age/data/ADNI/test_train_MRI_NP.csv", row.names = FALSE)
+write.csv(df, "C:/Users/doeringe/Documents/BrainAge/PET_MRI_age/data/merged/test_train_PET_42_NP.csv", row.names = FALSE)
