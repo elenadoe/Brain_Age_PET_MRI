0ec66be292702e15f3eaba360956706d2366ac54
antogeo <g.antonopoulos@fz-juelich.de>
2023-02-07 14:16:23 +0100

    add AAL parcelation code and csv

---
+++ src/0_ADNI parcelation/0_nilearn_parcelation2-mriADNI_nearestdate_AAL.py
@@ -0,0 +1,106 @@
+import os
+from nilearn._utils import check_niimg
+from nilearn.input_data import NiftiLabelsMasker
+import nibabel as nib
+import os.path as op
+import numpy as np
+import pandas as pd
+from glob import glob
+from nilearn.datasets import fetch_atlas_schaefer_2018
+from datetime import datetime
+import re
+
+
+def nearest(items, pivot):
+    return min(items, key=lambda x: abs(x - pivot))
+
+
+# fetch AAL atlas and labels
+atlas = 'data/0_ATLAS/AAL3v1_1mm.nii'
+atlas = nib.load(atlas)
+label_file = 'data/0_ATLAS/AAL3v1_1mm.nii.txt'
+label_list = open(label_file)
+label_elems = label_list.read().split('\n')
+labels = [x.split(' ')[1] for x in label_elems if len(x.split(' '))>1]
+# remove labels that were redefined in AAL3 and left empty for comparability
+exclude = ['Cingulate_Ant_L', 'Cingulate_Ant_R', 
+            'Thalamus_L', 'Thalamus_R']
+labels = [i for i in labels if i not in exclude]
+
+# this should include subjects' folders
+data_file = '/data/project/cat_12.5/ADNI_complete'
+
+# Edit paths before running
+groups = ['CN', 'MCI', 'SMC']
+for group in groups:  
+    f_path = 'data/ADNI/' + group + '/ADNI_PET_' + group + '_AAL3_1mm_parcels.csv'
+    subject_list = pd.read_csv(f_path)
+    subject_list = subject_list[subject_list.Group == group]
+    subject_list.name
+
+    output_csv = 'data/ADNI/{}/ADNI_MRI_{}_AAL3_1mm_parcels.csv'.format(group, group)
+
+    # ids to be excluded
+    excl_ids = []
+
+    # read IDs and age
+    subjs = subject_list[['age', 'name', 'sess']]
+
+    # subj_list = ['sub-' + sub.replace('_', '') for sub in subjs['name']]
+
+    image_list = []
+    subj_succ = {}
+    subj_succ['name'] = []
+    subj_succ['sess'] = []
+    subj_succ['Age'] = []
+    # subj_succ['age'] = []
+
+    # create list of regional data and subject IDs
+    for sub in subjs['name']:
+        sub_name = 'sub-' + sub.replace('_', '')
+        year = subjs[subjs['name'] == sub]['sess'].values[0].astype(str)[0:4]
+        day = subjs[subjs['name'] == sub]['sess'].values[0].astype(str)[4:6]
+        month = subjs[subjs['name'] == sub]['sess'].values[0].astype(str)[6:8]
+        pet_date = datetime(int(year), int(month), int(day), 0, 0)
+        fois = glob(op.join(data_file, sub_name, 'ses-' + '*', 'mri', '*.nii*'))
+        if fois:
+            datetimeobject = []
+            for foi in fois:
+                result = re.search('ses-(.*)/mri', foi)
+                datetimeobject.append(datetime.strptime(result.group(1)[0:8],
+                                    '%Y%m%d'))
+        nearest_scan = nearest(datetimeobject, pet_date)
+        nearest_foi = glob(op.join(data_file, sub_name, 'ses-' + str(
+                                    nearest_scan.year) + '*' + str(
+                                    nearest_scan.month) + '*' + str(
+                                    nearest_scan.day) + '??????', 'mri', '*.nii*'))
+        for near_foi in nearest_foi:
+            this_image = nib.load(near_foi)
+            path = os.path.normpath(near_foi)
+            sess = [dir for dir in path.split(os.sep) if dir.startswith('ses')]
+
+            niimg = check_niimg(this_image, atleast_4d=True)
+            masker = NiftiLabelsMasker(labels_img=atlas,
+                                    standardize=False,
+                                    memory='nilearn_cache',
+                                    resampling_target='labels')
+            parcelled = masker.fit_transform(niimg)
+            image_list.append(parcelled)
+            subj_succ['sess'].append(sess[0])
+            # subj_succ['age'].append()
+            subj_succ['name'].append(sub_name)
+            new_age = subjs[subjs['name'] == sub][
+                                    'age'].values[0] + (nearest_scan.year - int(year))
+            subj_succ['Age'].append(new_age)
+
+    features = np.array(image_list)
+    x, y, z = features.shape
+    features = features.reshape(x, z)
+    df = pd.DataFrame(features, columns=labels)
+    df_sub = pd.DataFrame(subj_succ)
+    df_final = pd.concat([df_sub, df], axis=1)
+
+    for excl_id in excl_ids:
+        df_final.drop(df_final[df_final['name'] == excl_id].index, inplace=True)
+
+    df_final.to_csv(output_csv, index=False)
