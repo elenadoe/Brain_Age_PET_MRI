565a84974bb0da3ce67fa1f0f9e46979f2dbe727
Elena Doering <doeringe@mmni-compute>
2023-03-23 16:51:45 +0100

    make atlas plot

--- src/graphics_create_plottable_atlas.py
+++ src/graphics_create_plottable_atlas.py
@@ -10,8 +10,10 @@ Created on Thu Mar 23 15:58:02 2023
 # look very big, which is misleading
 import numpy as np
 import nibabel as nib
+import matplotlib.pyplot as plt
 from nilearn import image
-from nilearn.datasets import fetch_atlas_aal
+from nilearn.datasets import fetch_atlas_aal, fetch_surf_fsaverage
+from nilearn import plotting, surface
 
 # get atlas
 atlas = fetch_atlas_aal()
@@ -27,4 +29,9 @@ mat[mat >= 9000] = 0
 
 atlas_for_plotting = image.new_img_like(atlas_nii, mat)
 
-nib.save(atlas_for_plotting, "../data/0_ATLAS/plottable_atlas.nii")
+surf = fetch_surf_fsaverage()
+mesh = surface.vol_to_surf(atlas_for_plotting, surf['pial_left'])
+plotting.plot_surf_roi(surf['pial_left'], mesh, view='lateral',
+                       hemi='left', bg_map = surf['sulc_left'],
+                       bg_on_data=True, darkness=0.2)
+plt.savefig("../data/0_ATLAS/graphics_AAL_parcellation.png", dpi=300)
